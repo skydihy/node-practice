b8ac33a7b13347fc744a526a8f144da2240f4918
skydihy <guylaxy.dev@gmail.com>
2022-11-07 15:49:33 +0700

    Update: create launches model and get controller

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+const morgan = require("morgan");
 const planetsRouter = require("../routes/planets/planets.router");
+const launchesRouter = require("../routes/launches/launches.router");
 
 const CORS_WHITELIST = ["http://localhost:3000"];
 
 const app = express();
 // Middlewares
 app.use(cors({ origin: CORS_WHITELIST }));
+app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(planetsRouter);
+app.use(launchesRouter);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
