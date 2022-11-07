e2e6fc4bdb648c528fef32b958c20b0e3b7609da
skydihy <guylaxy.dev@gmail.com>
2022-11-08 00:08:24 +0700

    Update: server - validations

--- server/routes/launches/launches.controller.js
+++ server/routes/launches/launches.controller.js
@@ -5,12 +5,31 @@ function httpGetAllLaunches(req, res) {
 }
 
 function httpAddNewLaunch(req, res) {
+  const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.destination
+  ) {
+    return res.status(400).json({
+      error: "Missing required launch property",
+    });
+  }
+
+  if (isNaN(new Date(launch.launchDate))) {
+    return res.status(400).json({
+      error: "Invalid launch date",
+    });
+  }
+
   let newLaunch = {
-    ...req.body,
+    ...launch,
     launchDate: new Date(req.body.launchDate),
   };
 
-  addNewLaunch(newLaunch)
+  addNewLaunch(newLaunch);
   return res.status(201).json(newLaunch);
 }
 
