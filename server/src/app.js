0f6dfd1af7b8ae4af42a430df872d29d61499d82
skydihy <guylaxy.dev@gmail.com>
2022-11-07 14:28:11 +0700

    Update: backend - setup routes, cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+const planetsRouter = require("../routes/planets/planets.router");
+
+const CORS_WHITELIST = ["http://localhost:3000"];
 
 const app = express();
 // Middlewares
+app.use(cors({ origin: CORS_WHITELIST }));
 app.use(express.json());
+app.use(planetsRouter);
 
 module.exports = app;
