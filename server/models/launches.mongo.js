453aaad0996779551233c2d4331e58d1ef0be751
skydihy <guylaxy.dev@gmail.com>
2022-11-12 15:53:29 +0700

    Update: create schema

---
+++ server/models/launches.mongo.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const launchesSchema = new mongoose.Schema({
+  flightNumber: {
+    type: Number,
+    required: true,
+  },
+  launchDate: {
+    type: Date,
+    required: true,
+  },
+  mission: {
+    type: string,
+    required: true,
+  },
+  rocket: {
+    type: string,
+    required: true,
+  },
+  target: {
+    type: String,
+    required: true,
+  },
+  customers: [String],
+  upcoming: {
+    type: Boolean,
+    required: true,
+  },
+  success: {
+    type: Boolean,
+    requred: true,
+    default: true,
+  },
+});
+
+module.exports = mongoose.model("Launch", launchesSchema);
