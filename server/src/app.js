e9a5399b2a3ff21e524dd3d45b40a546a45613de
skydihy <guylaxy.dev@gmail.com>
2022-11-12 15:35:09 +0700

    Update: setup mongo driver, env handler

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,6 @@
+require("dotenv").config({
+  path: process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : ".env",
+});
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,22 @@
 const http = require("http");
+const mongoose = require("mongoose");
 const app = require("./app");
 const { loadPlanetsData } = require("../models/planets.model");
+const { PORT, MONGO_URL } = require("../constants");
 
-const PORT = process.env.PORT || 8000;
-
+debugger;
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
